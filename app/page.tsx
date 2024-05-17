2ffafd84357dab4dd7567e740d55d3574cebd089
joao.lopes <joao.lopes@ocado.com>
2024-05-18 00:59:44 +0100

    Update eslint config

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Home(): React.JSX.Element {
     return (
         <main className={styles["main"]}>
             <h1>Hello world</h1>
