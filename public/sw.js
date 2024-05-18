c2ec0c66ba64fe45c58519b5c62044dda9354d51
joao.lopes <joao.lopes@ocado.com>
2024-05-18 21:17:19 +0100

    Update service worker script

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,25 @@
-const PWA_CACHE = "PWA_CACHE_{{BUILD_ID}}";
+const BUILD_ID = "{{BUILD_ID}}";
+const PWA_CACHE = `PWA_CACHE_${BUILD_ID}`;
 
 const assets = ["/", "/sw-register.js"];
 
 self.addEventListener("install", (event) => {
-    console.info("[SW] INSTALL");
+    console.info(`[SW-${BUILD_ID}] INSTALL`);
 
     event.waitUntil(
         caches
             .keys()
             .then(async (cacheKeys) => {
-                console.info("[SW] FOUND CACHES", cacheKeys);
+                console.info(`[SW-${BUILD_ID}] FOUND CACHES`, cacheKeys);
 
                 for (const key of cacheKeys) {
-                    console.info("[SW] DELETING OLD CACHE", key);
+                    console.info(`[SW-${BUILD_ID}] DELETING OLD CACHE`, key);
                     await caches.delete(key);
                 }
             })
             .then(() =>
                 caches.open(PWA_CACHE).then((cache) => {
-                    console.info("[SW] CACHING AT", PWA_CACHE);
+                    console.info(`[SW-${BUILD_ID}] ADDING ALL ASSETS`, assets);
                     return cache.addAll(assets);
                 })
             )
@@ -28,15 +29,18 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
     const url = new URL(event.request.url);
 
-    if (assets.includes(url.pathname) || url.pathname.startsWith("/_next/static/")) {
+    if (!assets.includes(url.pathname) && url.pathname.startsWith("/_next/static/")) {
+        assets.push(url.pathname);
+    }
+
+    if (assets.includes(url.pathname)) {
         event.respondWith(
             caches.match(event.request).then((cachedResponse) => {
                 const networkResponse = fetch(event.request)
                     .then((networkResponse) => {
                         if (networkResponse.ok) {
                             return caches.open(PWA_CACHE).then((cache) => {
-                                console.info("[SW] PWA_CACHE", PWA_CACHE);
-                                console.info("[SW] CACHING", url.pathname);
+                                console.info(`[SW-${BUILD_ID}] CACHING`, url.pathname);
                                 cache.put(event.request, networkResponse.clone());
                                 return networkResponse;
                             });
