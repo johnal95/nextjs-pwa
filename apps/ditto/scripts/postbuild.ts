d999a9e57cfb9e836ce82495367e3f3eb1aa2493
joao.lopes <joao.lopes@ocado.com>
2024-05-30 01:11:19 +0100

    Add postbuild script descriptive comments

--- apps/ditto/scripts/postbuild.ts
+++ apps/ditto/scripts/postbuild.ts
@@ -4,18 +4,23 @@ import path from "path";
 const NEXT_OUT_DIR = path.resolve(process.cwd(), ".next");
 const NEXT_STANDALONE_OUT_DIR = path.resolve(process.cwd(), ".next/standalone/apps/ditto");
 
+// COPY public folder into Next.js standalone build
+// * should move to CDN
 const publicAssets = {
     source: path.resolve(process.cwd(), "public"),
     target: path.resolve(NEXT_STANDALONE_OUT_DIR, "public"),
 };
 fs.cpSync(publicAssets.source, publicAssets.target, { recursive: true });
 
+// COPY static build folder into Next.js standalone build
+// * should move to CDN
 const staticOutput = {
     source: path.resolve(NEXT_OUT_DIR, "static"),
     target: path.resolve(NEXT_STANDALONE_OUT_DIR, ".next", "static"),
 };
 fs.cpSync(staticOutput.source, staticOutput.target, { recursive: true });
 
+// COMPILE sw.js
 const SW_PATH = path.resolve(publicAssets.target, "sw.js");
 const BUILD_ID = fs.readFileSync(path.resolve(NEXT_OUT_DIR, "BUILD_ID"), "utf-8");
 
